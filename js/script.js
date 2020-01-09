147989dc732597ce46c53a3f1be34af73268151b
sohxet <liandre@pm.me>
2020-01-09 23:37:55 +0200

    added css, js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+	// changes class name
+var doStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+	if( currentClassName == 'cool' ){
+		document.getElementById( 'cool' ).className = 'cool red';
+	} else {
+		document.getElementById( 'cool' ).className = 'cool';
+	}
+}
+
 var car = {
 	make: 'vw',
 	type: 'golf',
